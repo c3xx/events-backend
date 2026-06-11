9d07e09f4356bbd1e4adb18514d776d3f87fd0e8
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-06-11 20:50:06 +0530

    fix: include role information in step-roles

--- src/modules/workflow-template/repository.ts
+++ src/modules/workflow-template/repository.ts
@@ -1,4 +1,4 @@
-import { and, eq, isNull } from "drizzle-orm";
+import { and, eq, isNull, sql } from "drizzle-orm";
 import { db, schema } from "@/db/index.js";
 import { dbAction, unreachable } from "@/lib/helpers.js";
 
@@ -42,6 +42,42 @@ export const findById = dbAction(async (id: number) => {
 					id: true,
 					name: true,
 					nextStepId: true,
+				},
+				with: {
+					stepRoles: {
+						where: isNull(schema.workflowTemplateStepRole.deletedAt),
+						columns: {
+							targetGroupApprovalCriteria: true,
+						},
+						with: {
+							role: {
+								columns: {
+									id: true,
+									name: true,
+								},
+								extras: {
+									scope: sql<{
+										// note: null intentionally not handled because, critical system change
+										type: "organization" | "venue";
+										kindId: number;
+										kindName: string;
+									}>`case
+										when ${schema.role.managedEntityType} = 'organization'
+										then (
+											select json_build_object('type', ${schema.role.managedEntityType}, 'kindId', ot.id, 'kindName', ot.name)
+											from organization_type ot where ot.id = ${schema.role.typeRefId} limit 1
+										)
+										when ${schema.role.managedEntityType} = 'venue'
+										then (
+											select json_build_object('type', ${schema.role.managedEntityType}, 'kindId', vt.id, 'kindName', vt.name)
+											from venue_type vt where vt.id = ${schema.role.typeRefId} limit 1
+										)
+										else null
+									end`.as("scope"),
+								},
+							},
+						},
+					},
 				},
 			},
 		},
