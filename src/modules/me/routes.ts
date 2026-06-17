db3200377c8aa5ebc1026fd11285fcd54e765451
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-06-17 17:27:52 +0530

    fix import

--- src/modules/me/routes.ts
+++ src/modules/me/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import approvalAssignmentsRouter from "./approval-assignments/routes.js";
 import * as controller from "./controller.js";
-import rolesRouter from "./roles/routes.js";
 
 const router: Router = Router();
 
@@ -10,6 +9,5 @@ router.get("/", controller.userDetails);
 router.get("/organizations/event-creatable", controller.getEventCreatableOrganizations);
 
 router.use("/approval-assignments", approvalAssignmentsRouter);
-router.use("/roles", rolesRouter);
 
 export default router;
