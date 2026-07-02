a83ea52580aefdeb4d3bfcd2fea9df14fcd2b8d3
Dunkan <70066170+dcdunkan@users.noreply.github.com>
2026-07-02 19:56:38 +0530

    fix: incorrect controller linked to route

--- src/modules/me/invitation/routes.ts
+++ src/modules/me/invitation/routes.ts
@@ -4,7 +4,7 @@ import * as controller from "./controller.js";
 const router: Router = Router();
 
 router.get("/", controller.getPendingInvitations);
-router.get("/:invitationId", controller.getPendingInvitations);
+router.get("/:invitationId", controller.getPendingInvitation);
 router.patch("/:invitationId", controller.respondToInvitation);
 
 export default router;
